4a274eaad4cab629ae52f0f3d03548700ea6a7d9
Samuel Gonçalves <samuel.aires@tecnico.ulisboa.pt>
2024-12-06 12:24:18 +0000

    Small changes to cameras

--- test/Teste/src/mesh-loader.cpp
+++ test/Teste/src/mesh-loader.cpp
@@ -18,136 +18,148 @@
 ////////////////////////////////////////////////////////////////////////// MYAPP
 
 class MyApp : public mgl::App {
- public:
-  void initCallback(GLFWwindow *win) override;
-  void displayCallback(GLFWwindow *win, double elapsed) override;
-  void windowSizeCallback(GLFWwindow *win, int width, int height) override;
+public:
+    void initCallback(GLFWwindow* win) override;
+    void displayCallback(GLFWwindow* win, double elapsed) override;
+    void windowSizeCallback(GLFWwindow* win, int width, int height) override;
 
- private:
-  const GLuint UBO_BP = 0;
-  mgl::ShaderProgram *Shaders = nullptr;
-  mgl::Camera *Camera = nullptr;
-  GLint ModelMatrixId;
-  mgl::Mesh *Mesh = nullptr;
+private:
+    const GLuint UBO_BP = 0;
+    mgl::ShaderProgram* Shaders = nullptr;
+    std::vector<mgl::Camera*> Cameras;
+    GLint ModelMatrixId;
+    mgl::Mesh* Mesh = nullptr;
 
-  void createMeshes();
-  void createShaderPrograms();
-  void createCamera();
-  void drawScene();
+    void createMeshes();
+    void createShaderPrograms();
+    void createCameras();
+    void drawScene();
 };
 
 ///////////////////////////////////////////////////////////////////////// MESHES
 
 void MyApp::createMeshes() {
-  std::string mesh_dir = "models/";
-  // std::string mesh_file = "cube-v.obj";
-  // std::string mesh_file = "cube-vn-flat.obj";
-  // std::string mesh_file = "cube-vn-smooth.obj";
-  // std::string mesh_file = "cube-vt.obj";
-  // std::string mesh_file = "cube-vt2.obj";
-  // std::string mesh_file = "torus-vtn-flat.obj";
-  // std::string mesh_file = "torus-vtn-smooth.obj";
-  // std::string mesh_file = "suzanne-vtn-flat.obj";
-  // std::string mesh_file = "suzanne-vtn-smooth.obj";
-  // std::string mesh_file = "teapot-vn-flat.obj";
-  // std::string mesh_file = "teapot-vn-smooth.obj";
-  // std::string mesh_file = "bunny-vn-flat.obj";
-  std::string mesh_file = "bunny-vn-smooth.obj";
-  //std::string mesh_file = "monkey-torus-vtn-flat.obj";
-  std::string mesh_fullname = mesh_dir + mesh_file;
+    std::string mesh_dir = "models/";
+    // std::string mesh_file = "cube-v.obj";
+    // std::string mesh_file = "cube-vn-flat.obj";
+    // std::string mesh_file = "cube-vn-smooth.obj";
+    // std::string mesh_file = "cube-vt.obj";
+    // std::string mesh_file = "cube-vt2.obj";
+    // std::string mesh_file = "torus-vtn-flat.obj";
+    // std::string mesh_file = "torus-vtn-smooth.obj";
+    // std::string mesh_file = "suzanne-vtn-flat.obj";
+    // std::string mesh_file = "suzanne-vtn-smooth.obj";
+    // std::string mesh_file = "teapot-vn-flat.obj";
+    // std::string mesh_file = "teapot-vn-smooth.obj";
+    // std::string mesh_file = "bunny-vn-flat.obj";
+    std::string mesh_file = "bunny-vn-smooth.obj";
+    //std::string mesh_file = "monkey-torus-vtn-flat.obj";
+    std::string mesh_fullname = mesh_dir + mesh_file;
 
-  Mesh = new mgl::Mesh();
-  Mesh->joinIdenticalVertices();
-  Mesh->create(mesh_fullname);
+    Mesh = new mgl::Mesh();
+    Mesh->joinIdenticalVertices();
+    Mesh->create(mesh_fullname);
 }
 
 ///////////////////////////////////////////////////////////////////////// SHADER
 
 void MyApp::createShaderPrograms() {
-  Shaders = new mgl::ShaderProgram();
-  Shaders->addShader(GL_VERTEX_SHADER, "cube-vs.glsl");
-  Shaders->addShader(GL_FRAGMENT_SHADER, "cube-fs.glsl");
+    Shaders = new mgl::ShaderProgram();
+    Shaders->addShader(GL_VERTEX_SHADER, "cube-vs.glsl");
+    Shaders->addShader(GL_FRAGMENT_SHADER, "cube-fs.glsl");
 
-  Shaders->addAttribute(mgl::POSITION_ATTRIBUTE, mgl::Mesh::POSITION);
-  if (Mesh->hasNormals()) {
-    Shaders->addAttribute(mgl::NORMAL_ATTRIBUTE, mgl::Mesh::NORMAL);
-  }
-  if (Mesh->hasTexcoords()) {
-    Shaders->addAttribute(mgl::TEXCOORD_ATTRIBUTE, mgl::Mesh::TEXCOORD);
-  }
-  if (Mesh->hasTangentsAndBitangents()) {
-    Shaders->addAttribute(mgl::TANGENT_ATTRIBUTE, mgl::Mesh::TANGENT);
-  }
+    Shaders->addAttribute(mgl::POSITION_ATTRIBUTE, mgl::Mesh::POSITION);
+    if (Mesh->hasNormals()) {
+        Shaders->addAttribute(mgl::NORMAL_ATTRIBUTE, mgl::Mesh::NORMAL);
+    }
+    if (Mesh->hasTexcoords()) {
+        Shaders->addAttribute(mgl::TEXCOORD_ATTRIBUTE, mgl::Mesh::TEXCOORD);
+    }
+    if (Mesh->hasTangentsAndBitangents()) {
+        Shaders->addAttribute(mgl::TANGENT_ATTRIBUTE, mgl::Mesh::TANGENT);
+    }
 
-  Shaders->addUniform(mgl::MODEL_MATRIX);
-  Shaders->addUniformBlock(mgl::CAMERA_BLOCK, UBO_BP);
-  Shaders->create();
+    Shaders->addUniform(mgl::MODEL_MATRIX);
+    Shaders->addUniformBlock(mgl::CAMERA_BLOCK, UBO_BP);
+    Shaders->create();
 
-  ModelMatrixId = Shaders->Uniforms[mgl::MODEL_MATRIX].index;
+    ModelMatrixId = Shaders->Uniforms[mgl::MODEL_MATRIX].index;
 }
 
 ///////////////////////////////////////////////////////////////////////// CAMERA
 
 // Eye(5,5,5) Center(0,0,0) Up(0,1,0)
 const glm::mat4 ViewMatrix1 =
-    glm::lookAt(glm::vec3(5.0f, 5.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f),
-                glm::vec3(0.0f, 1.0f, 0.0f));
+glm::lookAt(glm::vec3(5.0f, 5.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 1.0f, 0.0f));
 
 // Eye(-5,-5,-5) Center(0,0,0) Up(0,1,0)
 const glm::mat4 ViewMatrix2 =
-    glm::lookAt(glm::vec3(-5.0f, -5.0f, -5.0f), glm::vec3(0.0f, 0.0f, 0.0f),
-                glm::vec3(0.0f, 1.0f, 0.0f));
+glm::lookAt(glm::vec3(-5.0f, -5.0f, -5.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 1.0f, 0.0f));
 
 // Orthographic LeftRight(-2,2) BottomTop(-2,2) NearFar(1,10)
 const glm::mat4 ProjectionMatrix1 =
-    glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, 1.0f, 10.0f);
+glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, 1.0f, 10.0f);
 
 // Perspective Fovy(30) Aspect(640/480) NearZ(1) FarZ(10)
 const glm::mat4 ProjectionMatrix2 =
-    glm::perspective(glm::radians(30.0f), 640.0f / 480.0f, 1.0f, 10.0f);
+glm::perspective(glm::radians(30.0f), 640.0f / 480.0f, 1.0f, 10.0f);
 
-void MyApp::createCamera() {
-  Camera = new mgl::Camera(UBO_BP);
-  Camera->setViewMatrix(ViewMatrix1);
-  Camera->setProjectionMatrix(ProjectionMatrix2);
+void MyApp::createCameras() {
+
+    mgl::Camera *Camera1 = new mgl::Camera(UBO_BP);
+    Camera1->setViewMatrix(ViewMatrix2);
+    Camera1->setProjectionMatrix(ProjectionMatrix1);
+    Cameras.push_back(Camera1);
+
+    mgl::Camera* Camera2 = new mgl::Camera(UBO_BP);
+    Camera2->setViewMatrix(ViewMatrix2);
+    Camera2->setProjectionMatrix(ProjectionMatrix1);
+    Cameras.push_back(Camera1);
 }
+
+
+
+
+
 
 /////////////////////////////////////////////////////////////////////////// DRAW
 
 glm::mat4 ModelMatrix(1.0f);
 
 void MyApp::drawScene() {
-  Shaders->bind();
-  glUniformMatrix4fv(ModelMatrixId, 1, GL_FALSE, glm::value_ptr(ModelMatrix));
-  Mesh->draw();
-  Shaders->unbind();
+    Shaders->bind();
+    glUniformMatrix4fv(ModelMatrixId, 1, GL_FALSE, glm::value_ptr(ModelMatrix));
+    Mesh->draw();
+    Shaders->unbind();
 }
 
 ////////////////////////////////////////////////////////////////////// CALLBACKS
 
-void MyApp::initCallback(GLFWwindow *win) {
-  createMeshes();
-  createShaderPrograms();  // after mesh;
-  createCamera();
+void MyApp::initCallback(GLFWwindow* win) {
+    createMeshes();
+    createShaderPrograms();  // after mesh;
+    createCameras();
 }
 
-void MyApp::windowSizeCallback(GLFWwindow *win, int winx, int winy) {
-  glViewport(0, 0, winx, winy);
-  // change projection matrices to maintain aspect ratio
+void MyApp::windowSizeCallback(GLFWwindow* win, int winx, int winy) {
+    glViewport(0, 0, winx, winy);
+    // change projection matrices to maintain aspect ratio
 }
 
-void MyApp::displayCallback(GLFWwindow *win, double elapsed) { drawScene(); }
+void MyApp::displayCallback(GLFWwindow* win, double elapsed) { drawScene(); }
 
 /////////////////////////////////////////////////////////////////////////// MAIN
 
-int main(int argc, char *argv[]) {
-  mgl::Engine &engine = mgl::Engine::getInstance();
-  engine.setApp(new MyApp());
-  engine.setOpenGL(4, 6);
-  engine.setWindow(800, 600, "Mesh Loader", 0, 1);
-  engine.init();
-  engine.run();
-  exit(EXIT_SUCCESS);
+int main(int argc, char* argv[]) {
+    mgl::Engine& engine = mgl::Engine::getInstance();
+    engine.setApp(new MyApp());
+    engine.setOpenGL(4, 6);
+    engine.setWindow(800, 600, "Mesh Loader", 0, 1);
+    engine.init();
+    engine.run();
+    exit(EXIT_SUCCESS);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
